7683d92382c1d8e5dcf9693dce3d1fe418128fa5
tahzeebsat <158441888+tahzeebsat@users.noreply.github.com>
2025-04-14 15:14:21 +0500

    Fixed minor bugs

--- resources/views/pdf/share_sale_invoice.blade.php
+++ resources/views/pdf/share_sale_invoice.blade.php
@@ -194,19 +194,19 @@
     						<div class="head-text">
     							<h3>Postal Address: </h3>
     							<p contenteditable="true" >P.O Box 2457 GPO, Sialkot (51310) Pakistan. <br>
-    							Tel: +92-300-6464270, Fax: +92-52-6618150
+    							Tel : +92-300-6464270, Fax : +92-52-6618150
     							</p>
     						</div>
     						<div class="party">
-    							<h3 contenteditable="true">PERFORMA INVOICE: <span style="font-size:15px;;" class="editable">{{ $saleInvoice->customer->full_name }}</span></h3>
+    							<h3 contenteditable="true">PERFORMA INVOICE : <span style="font-size:15px;;" class="editable">{{ $saleInvoice->customer->full_name }}</span></h3>
     							<div contenteditable="true" style="width:100%; overflow-wrap: break-word;" class="editable">
-                                  <b>  SHIP TO:</b> {{ $saleInvoice->customer->address}} <br>
+                                  <b>  SHIP TO : </b> {{ $saleInvoice->customer->address}} <br>
                                 </div>
                                 <div contenteditable="true" class="editable" style="width:100%; overflow-wrap: break-word;" rows="3">
 									{{ $saleInvoice->customer->post_code }} {{ $saleInvoice->customer->city }}, {{ $saleInvoice->customer->state }}, {{ $saleInvoice->customer->country }}
                                 </div>
 
-    							<strong contenteditable="true">Phone: </strong> {{ $saleInvoice->customer->phone }}<span></span>
+    							<strong contenteditable="true" class="editable">Phone : </strong> <span contenteditable="true">{{ $saleInvoice->customer->phone }}</span>
     						</div>
     					</td>
     					<td align="center"  style="padding:0px 0 0 0;">
@@ -215,27 +215,27 @@
     					</td>
     					<td width="33.33%" style="padding:13px 0 0 0;">
     						<div class="head-text" style="text-align:right" contenteditable="true">
-    							<h3>Factory Address: </h3>
+    							<h3>Factory Address : </h3>
     							<p  >New Abadi Sohawa, Circular Road <br> Daska 51010, Dist. Sialkot - Pakistan <br>
     							</p>
     						</div>
     						<div class="party" style="padding:0px 0 0 0; overflow:hidden;">
 									<div style="display: flex;">
-										<div contenteditable="true" style="font-weight:bold; width:-webkit-fill-available; padding:8px; border-bottom:1px solid #000; border-right:1px solid #000; width: 140px;" class="editable"><strong>DATE:</strong> {{ \Carbon\Carbon::parse($saleInvoice->invoice_date)->format('Y-m-d') }}</div>
-										<div contenteditable="true" style="font-weight:normal; width:-webkit-fill-available; padding:8px; border-bottom:1px solid #000; width:170px;" class="editable"><strong>TERM:</strong> {{ $saleInvoice->term }}</div>
+										<div contenteditable="true" style="font-weight:bold; width:-webkit-fill-available; padding:8px; border-bottom:1px solid #000; border-right:1px solid #000; width: 140px;" class="editable"><strong>DATE : </strong> {{ \Carbon\Carbon::parse($saleInvoice->invoice_date)->format('Y-m-d') }}</div>
+										<div contenteditable="true" style="font-weight:normal; width:-webkit-fill-available; padding:8px; border-bottom:1px solid #000; width:170px;" class="editable"><strong>TERM : </strong> {{ $saleInvoice->term }}</div>
 									</div>
 									<div style="display: flex;">
-                                    	<div contenteditable="true" class="editable" id="" style="width:-webkit-fill-available; padding:8px; border-bottom:1px solid #000; border-right:1px solid #000; width: 140px;" rows="3"><strong>Invoice:</strong> {{ $saleInvoice->invoice_number }}</div>
-										<div contenteditable="true" style="font-weight:normal; width:-webkit-fill-available; padding:8px; border-bottom:1px solid #000; width:170px;" class="editable"><strong>P.O. No:</strong>{{ (!empty($saleInvoice->po_no)) ? $saleInvoice->po_no : 'N/A' }}</div>
+                                    	<div contenteditable="true" class="editable" id="" style="width:-webkit-fill-available; padding:8px; border-bottom:1px solid #000; border-right:1px solid #000; width: 140px;" rows="3"><strong>Invoice : </strong> {{ $saleInvoice->invoice_number }}</div>
+										<div contenteditable="true" style="font-weight:normal; width:-webkit-fill-available; padding:8px; border-bottom:1px solid #000; width:170px;" class="editable"><strong>P.O : </strong>{{ (!empty($saleInvoice->po_no)) ? $saleInvoice->po_no : 'N/A' }}</div>
 									</div>
 									<div style="display: flex;">
-                                    	<div contenteditable="true" class="editable" id="" style="width:-webkit-fill-available; padding:8px; border-bottom:1px solid #000; border-right:1px solid #000; width: 140px;" rows="3"><strong>NTN No:</strong> 2130732-6</div>
-										<div contenteditable="true" style="font-weight:normal; width:-webkit-fill-available; padding:8px; border-bottom:1px solid #000; width:170px;" class="editable"><strong>BANK:</strong>{{ $saleInvoice->bank_name }}</div>
+                                    	<div contenteditable="true" class="editable" id="" style="width:-webkit-fill-available; padding:8px; border-bottom:1px solid #000; border-right:1px solid #000; width: 140px;" rows="3"><strong>NTN No : </strong> 2130732-6</div>
+										<div contenteditable="true" style="font-weight:normal; width:-webkit-fill-available; padding:8px; border-bottom:1px solid #000; width:170px;" class="editable"><strong>BANK : </strong>{{ $saleInvoice->bank_name }}</div>
 									</div>
 									<div style="display: flex;">
-                                    	<div contenteditable="true" class="editable" id="" style="width:-webkit-fill-available; padding:8px; border-right:1px solid #000; width: 140px;" rows="3"><strong>HS CODE:</strong> {{ $saleInvoice->hs_code }}</div>
+                                    	<div contenteditable="true" class="editable" id="" style="width:-webkit-fill-available; padding:8px; border-right:1px solid #000; width: 140px;" rows="3"><strong>HS CODE : </strong> {{ $saleInvoice->hs_code }}</div>
 										<div contenteditable="true" style="font-weight:normal; width:-webkit-fill-available; padding:8px; border-bottom:1px solid #000; width:170px;" class="editable">
-                                            <strong>Shipping:</strong>{{ $saleInvoice->shipping }}
+                                            <strong>Shipping: </strong>{{ $saleInvoice->shipping }}
                                         </div>
 
 									</div>
@@ -257,15 +257,15 @@
                 					<td width="25%"><strong>DATE: </strong> <input type="text" value="2019-01-02" style="border:none; padding:0px; margin:0px; font-size: 14px; width:191px;"></td>
                 					<td width="25%"><strong>INVOICE: </strong><input type="text" class="editable" value="VES-1323"></td>
                 					<td width="25%"><strong>NTN No.: </strong>2130732-6</td>
-                					<td width="25%"><strong>HS CODE:</strong> <input type="text" value="9022-2100" style="border:none; padding:0px; margin:0px; font-size: 14px;"></td>
+                					<td width="25%"><strong>HS CODE: </strong> <input type="text" value="9022-2100" style="border:none; padding:0px; margin:0px; font-size: 14px;"></td>
                 				</tr> -->
                 				<tr>
                 					<!-- <td width="25%" contenteditable="true"><strong>TERM: </strong><br />Payment terms</td> -->
                 					<!-- <td width="33.10%"><strong>BANK: </strong><br />Habib Bank Limited</td> -->
-                					<td width="50.00%" contenteditable="true"><strong>FINANCIAL INSTRUMENT NO: </strong>{{ $saleInvoice->financial_instrument_no }}</td>
-                                    <td width="25.00%" contenteditable="true"><strong>Port of Loading: </strong>{{ $saleInvoice->port_of_loading }}</td>
-                                    <td width="25.00%" contenteditable="true"><strong>Port of Discharge: </strong>{{ $saleInvoice->port_of_discharge }}</td>
-                					<!-- <td width="33.40%"><strong>P.O No:</strong><br /><input type="text" value="PO-123" style="border:none; padding:0px; margin:0px; font-size: 14px;"></td> -->
+                					<td width="50.00%" contenteditable="true"><strong>FINANCIAL INSTRUMENT NO : </strong>{{ $saleInvoice->financial_instrument_no }}</td>
+                                    <td width="25.00%" contenteditable="true"><strong>Port of Loading : </strong>{{ $saleInvoice->port_of_loading }}</td>
+                                    <td width="25.00%" contenteditable="true"><strong>Port of Discharge : </strong>{{ $saleInvoice->port_of_discharge }}</td>
+                					<!-- <td width="33.40%"><strong>P.O No: </strong><br /><input type="text" value="PO-123" style="border:none; padding:0px; margin:0px; font-size: 14px;"></td> -->
                 				</tr>
                 			</table>
     				    </td>
@@ -316,16 +316,16 @@
                                         </div>
 
                     					</td>
-                    					<td colspan="2"><strong>Freight Charges: </strong></td>
+                    					<td colspan="2"><strong>Freight Charges : </strong></td>
                     					<td><input id="freight_amount" class="update" type="text" style="border:none; padding:0px; margin:0px; font-size: 14px; width:100%" value="{{ $saleInvoice->freight_charges }}  {{ $saleInvoice->customer->currency ?? 'USD' }}"></td>
                     				</tr>
                     				<tr>
-                    					<td colspan="2"><strong>Bank Charges: </strong></td>
+                    					<td colspan="2"><strong>Bank Charges : </strong></td>
                     					<td>        <input id="tax_amount" class="update" type="text" style="border:none; padding:0px; margin:0px; font-size: 14px; width:100%" value="0.00  {{ $saleInvoice->customer->currency ?? 'USD' }}">
                                         </td>
                     				</tr>
                     				<tr>
-                    					<td colspan="2"><strong>Grand Total: </strong></td>
+                    					<td colspan="2"><strong>Grand Total : </strong></td>
                     					<td><input id="final_amount" type="text" style="border:none; padding:0px; margin:0px; font-size: 14px; width:100%" value="{{ $saleInvoice->items->sum('total_price') + $saleInvoice->tax_charges + $saleInvoice->freight_charges }}  {{ $saleInvoice->customer->currency ?? 'USD' }}"></td>
                     				</tr>
 
@@ -362,7 +362,7 @@
 													</tr>
 													@endforeach
 													<tr>
-														<td colspan="3" align="right"><strong>Total Payments:</strong></td>
+														<td colspan="3" align="right"><strong>Total Payments: </strong></td>
 														<td><strong>{{ $saleInvoice->payments->sum('amount') }}</strong></td>
 													</tr>
 												</tbody>
